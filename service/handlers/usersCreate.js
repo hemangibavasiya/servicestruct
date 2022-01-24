ca71b5ff3fa57375b272142bfc067e287cd27d25
hemangibavasiya <hemangibavasiya08@gmail.com>
2022-01-24 17:38:58 +0530

    fix: fix issue

--- service/handlers/usersCreate.js
+++ service/handlers/usersCreate.js
@@ -2,7 +2,7 @@ const UserTable = process.env.USER_TABLE
 const { inserData } = require('../comman/repository')
 
 const insertUserDetails = async (event, context, callback) => {
-    const response  = new Promise(function (resolve, reject) {
+    const response  = new Promise(async function (resolve, reject) {
         const data = JSON.parse(event.body);
         const response = await inserData(UserTable, data)
         resolve({ statusCode: 200, body: response.data })
