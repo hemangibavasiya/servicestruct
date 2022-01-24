e19c581bcd8e812dddac4d1dc23ffcec5a8e3134
hemangibavasiya <hemangibavasiya08@gmail.com>
2022-01-24 14:25:17 +0530

    f commit

--- service/handler.js
+++ service/handler.js
@@ -2,7 +2,7 @@
 
 const demogetapi = async (event, context, callback) => {
   const response = {
-    statusCode: 200,
+    statusCode: 200, 
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
