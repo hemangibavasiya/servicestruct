ac693b1387a4a02dfb2b01af1e711a0f4a79be4b
hemangibavasiya <hemangibavasiya08@gmail.com>
2022-02-03 15:14:24 +0530

    add sample layer

--- service/handlers/graphqlDemo.js
+++ service/handlers/graphqlDemo.js
@@ -5,7 +5,6 @@ const main = async (event, context, callback) => {
    return await new Promise(async (resolve, reject) => {
        await graphql(schema, JSON.parse(event.body).query).then(
             result => {
-                console.log(JSON.stringify(result) , '--------------------result------------------')
                   return resolve({ statusCode: 200, body: JSON.stringify(result) },
                 error =>  reject({ errorMessage: error })
                 )}
